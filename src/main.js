fe0eab23585011b396bbb815a866afa9406d9a9d
freewinter <freewinter7@gmail.com>
2019-02-14 14:20:30 +0900

    Remove nice-scrollbar

--- src/main.js
+++ src/main.js
@@ -5,14 +5,12 @@ import axios from 'axios'
 import i18next from 'i18next';
 import VueI18Next from '@panter/vue-i18next';
 import numberFormat from 'vue-filter-number-format';
-import vueNiceScrollbar from './components/vue-nice-scrollbar'
 import VueClipboard from 'vue-clipboard2'
 import VueNativeSock from 'vue-native-websocket'
 import store from './store'
 
 Vue.use(VueI18Next)
 Vue.use(require('vue-moment'))
-Vue.use(vueNiceScrollbar)
 Vue.use(VueClipboard)
 if (window.ActiveXObject || !('ActiveXObject' in window)) {
   Vue.use(VueNativeSock, 'ws://api.nexbill.net:8080', { connectManually: true })
